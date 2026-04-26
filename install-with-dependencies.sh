5b563cb7d5a903900bd170ed18edbbd0d2a4e9b1
Christian Alfonso <christian.a.alfonso@outlook.com>
2026-04-25 23:50:46 -0700

    Install Neovim itself from the installation script

--- install-with-dependencies.sh
+++ install-with-dependencies.sh
@@ -1,3 +1,10 @@
+# Install Neovim from their example in documentation:
+# https://github.com/neovim/neovim/blob/master/INSTALL.md#pre-built-archives-1
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim-linux-x86_64
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+sudo rm -f nvim-linux-x86_64.tar.gz
+
 # Install Ripgrep and FD, which are both used by
 # Telescope and possibly other plugins
 apt install fd-find
